73b37492ad0d76ab38dad7ef751c6f37cede4d66
huangwei <676018683@qq.com>
2014-10-22 22:46:08 +0800

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
   CarrierWave.configure do |config|
     config.storage             = :qiniu
-    config.qiniu_access_key    = "F1j4qLKjTkEY50oDlWpV5_j1FQgaufJP-zJ25NNc"
-    config.qiniu_secret_key    = 'DtZJiyd1IUJX-e2N6Qtd1MfbMv_D7VpKVzGvbTyl'
+    config.qiniu_access_key    = "mzSexau9XTCpRFCw3tYRBO9LWWDmJ6T1_7orUJKZ"
+    config.qiniu_secret_key    = "OsYC_MVUBnpLMFvfCcpdkv2jbyN5qatkis6UvOuF"
     config.qiniu_bucket        = "shop-picture"
     config.qiniu_bucket_domain = "shop-picture.qiniudn.com"
     # config.qiniu_protocol      = 'https'
